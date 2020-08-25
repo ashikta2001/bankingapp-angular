08866cd2ee1269024951a129a08251a7c959aa5c
ashikta2001 <ashikta2001@gmail.com>
2020-08-25 13:33:09 +0400

    app component title changed spelling mistake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'banking applicatoin title';
+  title = 'banking application title';
 }
