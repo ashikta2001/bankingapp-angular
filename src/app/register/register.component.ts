78c9b755287a137509a1d36bc166a256f7a269a5
ashikta2001 <ashikta2001@gmail.com>
2020-09-07 13:20:22 +0400

    Reactive form builder and form validations added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,23 +9,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  acname = ""
-  acno = ""
-  acpin = ""
-  pwd =""
+  registerForm = this.fb.group(
+    {
+      acname:['',[Validators.required] ],
+      acno:['', [Validators.required, Validators.minLength(4)]],
+      acpin:['', [Validators.required]],
+      pwd:['', [Validators.required]],
+    }
+  )
 
   constructor(private dataService:DataService,
-    private router:Router) { }
+    private router:Router,
+    private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
 
   register(){
-    const result=this.dataService.register(this.acname, this.acno, this.acpin, this.pwd);
-    alert(result)
-    if (result){
-      alert("Successfully created account, Please login!!!");
-      this.router.navigateByUrl("");
+    if (this.registerForm.valid) {
+      const result=this.dataService.register(this.registerForm.value.acname, this.registerForm.value.acno, this.registerForm.value.acpin, this.registerForm.value.pwd);
+      alert(result)
+      if (result){
+        alert("Successfully created account, Please login!!!");
+        this.router.navigateByUrl("");
+      }
+    }
+    else{
+      alert("Validations Failed. Please re-enter form data")
     }
   }
 }
